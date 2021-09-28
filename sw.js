b71747bda9428b180a840875b163876005af36b0
Sam Crawford <email@samjcrawford.com>
2021-09-28 09:02:07 +0100

    Fix index problem?

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210928.0844';
+const cacheName = 'chirpy-20210928.0901';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
