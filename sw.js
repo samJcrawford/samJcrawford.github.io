3c5aab08e87e7da0eb0bb4918bcb79c651849f28
Sam Crawford <email@samjcrawford.com>
2021-09-28 09:27:43 +0100

    Might have forgotten to build

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210928.0922';
+const cacheName = 'chirpy-20210928.0927';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
