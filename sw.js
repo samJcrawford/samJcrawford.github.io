342a2d2bd44566abdcb5bf17097fae5f6675ce7e
Sam Crawford <email@samjcrawford.com>
2021-09-28 09:20:39 +0100

    Test

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210928.0916';
+const cacheName = 'chirpy-20210928.0920';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
